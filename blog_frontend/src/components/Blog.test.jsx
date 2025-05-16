b191ef988d8ebb4676d11f05e9c75197ca67fa48
Sephydev <nicolas.bussiere@outlook.fr>
2025-05-16 10:53:20 +0200

    Add test checking View Details functionality

--- blog_frontend/src/components/Blog.test.jsx
+++ blog_frontend/src/components/Blog.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-import { expect, vi } from 'vitest'
 
 test('<Blog /> initially render only title and author', () => {
   const blog = {
@@ -19,10 +19,34 @@ test('<Blog /> initially render only title and author', () => {
 
   const element = screen.getByText(/Test Title/)
 
-  screen.debug()
-
   expect(element).toBeDefined()
   expect(element).toHaveTextContent(blog.author)
   expect(element).not.toHaveTextContent(blog.url)
   expect(element).not.toHaveTextContent(blog.likes)
+})
+
+test('<Blog /> show URL and Like when show button is clicked', async () => {
+  const blog = {
+    title: 'Test Title',
+    author: 'Sephydev',
+    url: 'http://www.test.com/',
+    likes: 0,
+    user: {
+      username: 'Sephydev'
+    }
+  }
+
+  const user = userEvent.setup()
+
+  const mockUser = vi.fn()
+
+  render(<Blog blog={blog} user={mockUser} />)
+
+  const element = screen.getByText(/Test Title/)
+  const viewButton = screen.getByText('view')
+
+  await user.click(viewButton)
+
+  expect(element).toHaveTextContent(blog.url)
+  expect(element).toHaveTextContent(blog.likes)
 })
